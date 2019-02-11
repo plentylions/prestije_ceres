9ed799bf5c0c5aebd946c1b0360c82f665e04d39
Misha Yatsenko <1234asdfm@gmail.com>
2019-02-11 13:11:40 +0200

    fix

--- resources/js/script.js
+++ resources/js/script.js
@@ -5,13 +5,4 @@ $(function () {
   $('#video-modal').on('hidden.bs.modal', function () {
     $('#video-modal .video-popup-video').trigger('pause');
   });
-
-  $(document).on('click', '.mobile-navigation', function () {
-    let navDirection = $(this).parent().find('.nav-direction');
-    let hasChild = navDirection.length > 0;
-    if (hasChild) {
-      navDirection.trigger('click');
-      return false;
-    }
-  });
 });
