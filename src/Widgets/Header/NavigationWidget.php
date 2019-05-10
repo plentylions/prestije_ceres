33c1d6155c29f4f8a7cf334b1fde2048046c8136
plentylionsgit <info@plentylions.de>
2019-05-10 13:50:41 +0300

    dev

---
+++ src/Widgets/Header/NavigationWidget.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace PrestijeLegend\Widgets\Header;
+
+use Ceres\Widgets\Helper\BaseWidget;
+
+class NavigationWidget extends BaseWidget
+{
+    protected $template = "PrestijeLegend::Widgets.Header.NavigationWidget";
+}
