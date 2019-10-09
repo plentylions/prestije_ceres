237dc9c0f598b74b080ccff397c4375bfb97142a
plentylionsgit <info@plentylions.de>
2019-10-09 11:00:39 +0300

    dev

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,24 +1,136 @@
+Vue.component("carousel", {
+
+  components: {
+    SlotComponent: {
+      functional: true,
+      render: (createElement, context) => context.props.vnode
+    }
+  },
+
+  props: {
+    template: {
+      type: String,
+      default: "#vue-carousel"
+    },
+    itemsPerPage: {
+      type: Number,
+      default: 4
+    }
+  },
+
+  data() {
+    return {
+      itemCount: 0
+    };
+  },
+
+  computed: {
+    columnWidths() {
+      let itemsPerPage = this.itemsPerPage;
+
+      if (itemsPerPage < 1) {
+        itemsPerPage = 1;
+      } else if (itemsPerPage > 4) {
+        itemsPerPage = 4;
+      }
+
+      return [
+        itemsPerPage === 1 ? "col-12" : "col-6",
+        itemsPerPage === 1 ? "col-sm-12" : "col-sm-6",
+        "col-md-" + (12 / itemsPerPage)
+      ];
+    }
+  },
+
+  created() {
+    if (this.$slots.items) {
+      this.itemCount = this.$slots.items.length;
+    }
+  },
+
+  mounted() {
+    this.$nextTick(() => {
+      if (this.itemCount > this.itemsPerPage) {
+        this.initializeCarousel();
+      }
+    });
+  },
+
+  methods: {
+    initializeCarousel() {
+      const self = this;
+
+      $(this.$refs.carouselContainer).owlCarousel({
+        autoHeight: true,
+        dots: true,
+        items: self.itemsPerPage,
+        responsive: {
+          0: {
+            items: self.itemsPerPage > 1 ? 2 : 1
+          },
+          576: {
+            items: self.itemsPerPage > 1 ? 2 : 1
+          },
+          768: {
+            items: self.itemsPerPage > 3 ? 3 : self.itemsPerPage
+          },
+          992: {
+            items: self.itemsPerPage
+          }
+        },
+        lazyLoad: false,
+        loop: false,
+        margin: 30,
+        mouseDrag: true,
+        nav: true,
+        navClass: [
+          "owl-single-item-nav left carousel-control list-control-special",
+          "owl-single-item-nav right carousel-control list-control-special"
+        ],
+        navContainerClass: "",
+        navText: [
+          "<i class=\"owl-single-item-control fa fa-chevron-left\" aria-hidden=\"true\"></i>",
+          "<i class=\"owl-single-item-control fa fa-chevron-right\" aria-hidden=\"true\"></i>"
+        ],
+        smartSpeed: 350,
+        onChanged: property => {
+          const begin = property.item.index;
+          const end = begin + property.page.size;
+
+          for (let i = begin; i < end; i++) {
+            const childComponent = self.$children[i];
+
+            if (childComponent && childComponent.loadFirstImage) {
+              childComponent.loadFirstImage();
+            }
+          }
+        }
+      });
+    }
+  }
+});
+
 var newVue;
 $(function () {
   function removeParam(key, sourceURL) {
     var rtn = sourceURL.split("?")[0],
-        param,
-        params_arr = [],
-        queryString = (sourceURL.indexOf("?") !== -1) ? sourceURL.split("?")[1] : "";
+      param,
+      params_arr = [],
+      queryString = (sourceURL.indexOf("?") !== -1) ? sourceURL.split("?")[1] : "";
     if (queryString !== "") {
-        params_arr = queryString.split("&");
-        for (var i = params_arr.length - 1; i >= 0; i -= 1) {
-            param = params_arr[i].split("=")[0];
-            if (param === key) {
-                params_arr.splice(i, 1);
-            }
+      params_arr = queryString.split("&");
+      for (var i = params_arr.length - 1; i >= 0; i -= 1) {
+        param = params_arr[i].split("=")[0];
+        if (param === key) {
+          params_arr.splice(i, 1);
         }
-        rtn = rtn + "?" + params_arr.join("&");
+      }
+      rtn = rtn + "?" + params_arr.join("&");
     }
     return rtn;
   }
 
-  if( $('.product-list > li'). length > 0 ){
+  if ($('.product-list > li').length > 0) {
     $('ul.product-list').after('<div class="lds-ellipsis infiniteScrollLoader"><div></div><div></div><div></div><div></div></div>');
     let nextUrl = removeParam("page", window.location.href);
     if (nextUrl.indexOf('?') > -1) {
@@ -27,7 +139,7 @@ $(function () {
       nextUrl += '?page={{#}}';
     }
 
-    $( ".product-list" ).wrap( "<div class='inf-scroll-parent'></div>" );
+    $(".product-list").wrap("<div class='inf-scroll-parent'></div>");
 
     let $container = $('.product-list').parent();
     $container.infiniteScroll({
@@ -39,7 +151,7 @@ $(function () {
 
     $container.on('load.infiniteScroll', function (event, response, path) {
       let $items = $(response).find('.product-list > li');
-      if( $items.length == 0 ){ // Last
+      if ($items.length == 0) { // Last
         $container.infiniteScroll('destroy');
         $('.infiniteScrollLoader').remove();
       }
@@ -71,11 +183,11 @@ $(function () {
     var stickyHeaderTop = $('.top-bar .before-header').offset().top;
 
     $(window).scroll(function () {
-        if ($(window).scrollTop() > stickyHeaderTop) {
-            $('.top-bar .before-header').addClass('sticky');
-        } else {
-            $('.top-bar .before-header').removeClass('sticky');
-        }
+      if ($(window).scrollTop() > stickyHeaderTop) {
+        $('.top-bar .before-header').addClass('sticky');
+      } else {
+        $('.top-bar .before-header').removeClass('sticky');
+      }
     });
   }
 });
